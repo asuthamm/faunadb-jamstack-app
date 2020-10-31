6a1431011521d9bcd24fcdac89f3011d052bea0f
Alex Suthammanont <asuthamm@gmail.com>
2020-10-31 15:03:31 -0400

    update sendQuery

--- functions/utils/sendQuery.js
+++ functions/utils/sendQuery.js
@@ -2,9 +2,7 @@ const axios = require('axios');
 require('dotenv').config();
 
 module.exports = async (query, variables) => {
-    const {
-        data: { data, errors },
-    } = await axios({
+    const { data: { data, errors } } = await axios({
         url: 'https://graphql.fauna.com/graphql',
         method: 'POST',
         headers: {
